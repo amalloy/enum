93aab0978a0533b64edc51d4c30e1e4b06a5e825
Alan Malloy <alan@malloys.org>
2010-09-08 21:59:43 -0700

    use cons instead of list*, and add flip-map for possible future use

--- src/enum/core.clj
+++ src/enum/core.clj
@@ -1,12 +1,14 @@
 (ns enum.core)
 
+(def flip-map (comp (partial apply hash-map) flatten (partial map reverse)))
+
 (defmacro defenum [type def-prefix name1-or-map & more-names]
   (let [[to-id-sym from-id-sym] (map (comp symbol str)
 				     (repeat type)
 				     ["-id-from-text" "-text-from-id"])
 	name-hash (if (map? name1-or-map)
 		    name1-or-map
-		    (zipmap (list* name1-or-map more-names) (range)))
+		    (zipmap (cons name1-or-map more-names) (range)))
 	[names values] ((juxt keys vals) name-hash)
 	name-strs (map str names)]
     `(do
